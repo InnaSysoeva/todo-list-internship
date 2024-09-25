53e7ec9af5683651f7a9fc2b106ccddb768bb9e0
Your Name <you@example.com>
2024-09-25 12:44:48 +0300

    Task-1: FE: Setup project

---
+++ client/src/configs/vite.config.ts
@@ -0,0 +1,21 @@
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+    "@": path.resolve(__dirname, "src"),
+    "@components": path.resolve(__dirname, 'src/main/components'),
+    "@services": path.resolve(__dirname, 'src/services'),
+    "@constants": path.resolve(__dirname, 'src/main/constants'),
+    "@hooks": path.resolve(__dirname, 'src/main/hooks'),
+    "@models": path.resolve(__dirname, 'src/main/models'),
+    "@layouts": path.resolve(__dirname, 'src/main/layouts'),
+    "@enums": path.resolve(__dirname, 'src/main/enums'),
+    "@api": path.resolve(__dirname, 'src/main/api'),
+    "@styles": path.resolve(__dirname, 'src/styles'),
+    "@utils": path.resolve(__dirname, 'src/utils'),
+  }},
+});
