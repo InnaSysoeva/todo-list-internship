2e0e20fc6f684858d6c1ce02f54b9422b4ab2d68
InnaSysoeva <inna.sysoeva56@gmail.com>
2024-09-26 16:01:39 +0300

    Task-4: BE: Refactored data base connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,21 +2,18 @@ import dotenv from "dotenv";
 import express, { Request, Response } from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import connectDB from './configs/db.connection'
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 const defaultPort = 5000;
 const port = process.env.PORT || defaultPort;
-const mongoURI = process.env.MONGO_URI || '';
 
 app.use(cors());
 app.use(express.json());
-
-
-mongoose.connect(mongoURI)
-  .then(() => console.log("Connected to MongoDB Atlas"))
-  .catch((error) => console.log('MongoDb connection error: ', error))
 
 app.get("/", (request: Request, result: Response) => {
   result.send("Hello, World!");
