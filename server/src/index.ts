0349621d3bfc148547b3074cd6f914c3d77ff260
InnaSysoeva <inna.sysoeva56@gmail.com>
2024-09-26 09:49:39 +0300

    Task-3: BE: Setup DB connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,22 @@
 import dotenv from "dotenv";
 import express, { Request, Response } from "express";
+import cors from "cors";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const app = express();
 const defaultPort = 5000;
 const port = process.env.PORT || defaultPort;
+const mongoURI = process.env.MONGO_URI || '';
 
+app.use(cors());
 app.use(express.json());
+
+
+mongoose.connect(mongoURI)
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch((error) => console.log('MongoDb connection error: ', error))
 
 app.get("/", (request: Request, result: Response) => {
   result.send("Hello, World!");
