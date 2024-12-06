10040eefbf2f44fd8b72fe507d9ade800dbacc35
Inna Sysoeva <71695604+InnaSysoeva@users.noreply.github.com>
2024-12-06 12:00:17 +0200

    Update index.ts

--- client/src/main/api/index.ts
+++ client/src/main/api/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const $host = axios.create({
-  baseURL: "http://localhost:3000/api",
-  //baseURL: "https://todo-list-internship-server.onrender.com/api"
+  //baseURL: "http://localhost:3000/api",
+  baseURL: "https://todo-list-internship-server.onrender.com/api"
 });
